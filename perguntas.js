c39d0e499cdd92e4df3fbf8fddd0097c4c3975ee
Felipe Gabriel Souza Santos <0000110820031Xsp@al.educacao.sp.gov.br>
2024-11-18 09:05:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Saúde',
+    'O que é sedentarismo?',
+    'É considerada sedentária a pessoa que se mantém muito tempo inativa, sem praticar exercícios físicos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Jamaica?',
+    'A capital da Jamaica é Kingston'
+)
+
+criaCartao(
+    'Espotes',
+    'O que é um touchdown?',
+    'Um touchdown é uma forma de marcar pontos e acontece quando um jogador leva a bola até a endzone do oponente.'
+)
+
+criaCartao(
+    'História',
+    'Quando foi criado o Voleibol?',
+    'O voleibol foi criado em 1895'
+)
